653d19e0bc774419194e28bfc75bb4ef79c8e627
Deepak <dgusaiwal537@Gmail.com>
2023-01-28 22:49:38 +0530

    this is the last commit

--- index.js
+++ index.js
@@ -8,3 +8,16 @@
 //3. git add .  (To Add All The Files to The Staging Area)
 
 //4. git commit -m "initial commit"  (To Commit the the files from staging area)
+
+//then set you git origin and push your code to that
+
+// ====================================for more simplification 
+// create your repository at you github account and copy past this code in the terminal of your code in vs code
+// but change your origin url 
+// 5.
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Deepak-gusaiwal/git-command-line-code.git
+// git push -u origin main
