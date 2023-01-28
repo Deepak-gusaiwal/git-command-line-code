f3213845e8ddae7806aaf948060349426a601256
Deepak <dgusaiwal537@Gmail.com>
2023-01-28 22:24:57 +0530

    initial commite by Deepak

---
+++ index.js
@@ -0,0 +1,6 @@
+//1. To Config the GitHub for you
+// To Set Name Globally run     (git config --global user.name Deepak)
+// To Set Email Globally run     (git config --global user.email dgusaiwal537@gmail.com)
+// To See the Changes are maded successfully or not run this    (git config --global user.name) output == Deepak;
+
+//2. run git init To initialize a empty git repository
